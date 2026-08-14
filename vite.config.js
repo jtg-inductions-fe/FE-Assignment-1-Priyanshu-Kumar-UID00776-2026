ce795edd26983f231d68c5c1bd4fcafa981f5888
Priyanshu-workSpace <priyanshu.kumar@joshtechnologygroup.com>
2026-08-14 15:59:38 +0530

    [PK_A1_08]: Improved the SEO

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig(() => {
                 minify: true,
             }),
             ViteImageOptimizer({
-                test: /\.(jpg|png)$/i,
+                test: /\.(webp|png)$/i,
                 includePublic: true,
                 logStats: true,
                 png: {
@@ -29,7 +29,7 @@ export default defineConfig(() => {
                     chunkFileNames: 'js/[name]-[hash].js',
                     entryFileNames: 'js/[name]-[hash].js',
                     assetFileNames: ({ name }) => {
-                        if (/\.(jpg|png)$/.test(name ?? '')) {
+                        if (/\.(webp|png)$/.test(name ?? '')) {
                             return 'images/[name]-[hash][extname]';
                         }
                         if (/\.css$/.test(name ?? '')) {
